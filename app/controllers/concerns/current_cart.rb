b5652a2bd2ef9c2b778863b80100942c129624d0
trojanh <rajan.tiwari@kiprosh.com>
2016-12-06 13:49:22 +0530

    AWD in Rails till chapter 10

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,17 @@
+module CurrentCart
+  extend ActiveSupport::Concern
+    def increment_counter
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
+    end
+  private
+
+    def set_cart
+      @cart= Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      @cart=Cart.create
+      session[:cart_id]=@cart.id
+    end
+end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products= Product.order(:title)
+    # @counter = session[:counter]
+    # @counter.nil? ? @counter = 1 : @counter+=1
+    # session[:counter] = @counter
   end
 end
